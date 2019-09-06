6568d89abb1c73d04880f069b711f9406b4e9b16
Chaeyeon Won <zxz8091@naver.com>
2019-09-06 15:00:19 +0900

    여유, 보통, 혼잡 종쳤음. 반복하는거 추가하기

---
+++ 2학기/loose_normal_crowded.py
@@ -0,0 +1,21 @@
+#버스 여유, 보통, 혼잡
+#탑승객, 하차객 입력받자
+#0명 이상 10 미만 : 여유, 10명이상 20명 미만: 보통, 20이상: 혼잡
+
+#--------반복하기
+while True:
+    inn=int(input("탑승객 수는?:"))
+    out = int(input("하차객 수는?:"))
+    sum = inn-out
+    if inn == -1:
+        break
+#----
+
+print("버스에 있는 사람수는",sum)
+
+if 0 <= sum < 10:
+    print("[[여유]]")
+elif 10 <= sum <20:
+    print("[[보통]]")
+else:
+    print("[[혼잡]]")
