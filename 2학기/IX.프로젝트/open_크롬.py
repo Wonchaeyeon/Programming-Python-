97c678611edd43548fd0c7ca3366e7aa1cc89151
Wonchaeyeon <zxz8091@naver.com>
2019-12-06 14:52:49 +0900

    PyAutoGui - locateAllOnScreen()

--- 2학기/IX.프로젝트/open_크롬.py
+++ 2학기/IX.프로젝트/open_크롬.py
@@ -9,6 +9,9 @@ if __name__ == '__main__':
     #pag.moveTo(data.left+(data.width/2),date.top+(data.height/2))
     # center=pag.center(data)
     # print(center)
-    center=pag.locateCenterOnScreen("크롬.png")
-    pag.moveTo(center,duration=2)
-    pag.doubleClick()
+    # center=pag.locateCenterOnScreen("크롬.png")
+    # pag.moveTo(center,duration=2)
+    # pag.doubleClick()
+    datas=pag.locateAllOnScreen("크롬.png")   #특정 이미지 모두 가져와서 위치 찍기
+    for dat in datas:
+        print(data)
